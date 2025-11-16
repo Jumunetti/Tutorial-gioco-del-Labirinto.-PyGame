6bb95a68b24fc2e019750a6845032599d4c90113
luciroma8 <luciroma8@gmail.com>
2025-11-16 10:47:23 +0100

    esecizio1.py

--- lesson1/esecizio1.py
+++ lesson1/esecizio1.py
@@ -13,29 +13,27 @@ YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
-#TODO1: crea 2 Rect che si toccano oppure no
- 
-
- 
-dt = clock.tick(60) / 1000.0
+# TO DO 1 crea due rettangoli
+rect1 = pygame.Rect(200, 200, 100, 100)
+rect2 = pygame.Rect(350, 200, 100, 100)
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    keys = pygame.key.get_pressed()
 
-    #gestione dei collide
+    # Collisione
+    if rect1.colliderect(rect2):
+        print("I rettangoli si toccano!")
+    else:
+        print("I rettangoli NON si toccano.")
 
-    #TODO2:scivi ina condizione che scive se i 2 Rect si toccano
-    
- 
-    # Disegna tutto
+    # Disegno
     win.fill(BLACK)
+    pygame.draw.rect(win, RED, rect1)
+    pygame.draw.rect(win, BLUE, rect2)
 
-    #TODO3:scrivi la parte per far apparire a schermo i Rect
- 
     pygame.display.update()
     clock.tick(60)
  
